2f77a6c75387f6befaa5b3832847c80b2254dc89
hiimtmac <taylormcintyre_2@hotmail.com>
2020-01-25 16:12:11 -0600

    Initial Commit

---
+++ Racecar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Racecar
+//
+//  Created by Taylor McIntyre on 2020-01-25.
+//  Copyright © 2020 Taylor McIntyre. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
